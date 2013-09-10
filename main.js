140966d40aaf3a4668c534c05400fd3ee1acea47
yashihei <yashihei@gmail.com>
2013-09-10 22:07:01 +0900

    テトリスミノクラスとかブロックの配列を二次元化とか
    gitの使い方覚えたはいいけど、うまいコミットの仕方がわからないので意味がなすぎる
    ビジュアル面の変化がないからモチベが

--- main.js
+++ main.js
@@ -5,8 +5,8 @@
 /*
  * 定数
  */
-var SCREEN_WIDTH    = 480;
-var SCREEN_HEIGHT   = 480;
+var SCREEN_WIDTH    = 400;
+var SCREEN_HEIGHT   = 440;
 var SCREEN_CENTER_X = SCREEN_WIDTH/2;
 var SCREEN_CENTER_Y = SCREEN_HEIGHT/2;
 
@@ -15,13 +15,9 @@ var BLOCK_NUM_Y = 21;
 var BLOCK_NUM = BLOCK_NUM_X * BLOCK_NUM_Y;
 var BLOCK_SIZE = 20;//テトリスのブロックは縦横変わらないので
 
-var MINO_NUM = 4;
-var MINO_TYPE = [
-    0, 0, 0, 0,
-    0, 1, 1, 0,
-    0, 1, 1, 0,
-    0, 0, 0, 0,
-]
+var MINO_NUM_X = 4;
+var MINO_NUM_Y = 4;
+var MINO_NUM = 16;
 
 var COLOR = {RED: 0, BLUE: 1, GREEN: 2, YELLOW: 3};
 var TYPE = {NONE: 0, WALL: 1, BLOCK: 2};
@@ -52,28 +48,31 @@ tm.define("MainScene", {
         this.blockGroup = tm.app.CanvasElement();
         this.addChild(this.blockGroup);
 
-        this.blocks = [];
-        this.currentMino;
+        this.cnt = 0;//カウントでふ
+        this.blocks = new Array();
 
         //ブロック初期化
         for (var i = 0; i < BLOCK_NUM_Y; ++i) {
+            this.blocks[i] = new Array();
             for (var j = 0; j < BLOCK_NUM_X; ++j) {
-                var index = i * BLOCK_NUM_X + j;
-                this.blocks[index] = Block().addChildTo(this.blockGroup);
+                this.blocks[i][j] = Block().addChildTo(this.blockGroup);
                 if (j == 0 || j == BLOCK_NUM_X - 1 || i == BLOCK_NUM_Y - 1) {
-                    this.blocks[index].type = TYPE.WALL;
+                    this.blocks[i][j].type = TYPE.WALL;
                 }
-                this.blocks[index].x = (BLOCK_SIZE + 1) * j ;
-                this.blocks[index].y = (BLOCK_SIZE + 1) * i ;
-                console.log(this.blocks[index].x);
-                console.log(this.blocks[index].y);
+                //座標は左上ではなく中心っぽいのです ていうかサンプルの星で気づくべきだった…
+                //sprite.origin.x sprite.origin.yを設定することで左上を頂点とできるみたい
+                this.blocks[i][j].x = (BLOCK_SIZE + 1) * j + BLOCK_SIZE/2;
+                this.blocks[i][j].y = (BLOCK_SIZE + 1) * i + BLOCK_SIZE/2;
             }
         }
 
-        // this.currentMino = Mino();
-        // this.currentMino.flag = false;
-        // this.currentMino.status = 
-        // this.addChild(this.currentMino);
+        //this.mino = Mino(new Array(new Point(0, -1), new Point(1, -1), new Point(0, 0), new Point(1, 1)), 1, COLOR.RED);
+        this.minos = new Array(
+            Mino(new Array(new Point(0, -1), new Point(1, -1), new Point(0, 0), new Point(1, 0)), 1, COLOR.RED)
+        );
+
+        //ミノの場所
+        this.pos = new Point(4, 1);
 
         // 星スプライト
         this.star = tm.app.StarShape(64, 64);
@@ -89,6 +88,16 @@ tm.define("MainScene", {
         if (app.pointing.getPointing() == true) {
             this.star.rotation += 15;
         }
+
+        // this.cnt++;
+        // var nextPos = new Point(this.pos.x, this.pos.y);
+        // if ((this.cnt + 1) % 60 == 0) nextPos.y++;
+
+        // for (var i = 0; i < 4; i++) {
+        // }
+
+        //var time = ((app.frame / app.fps) * 1000)|0;
+        //console.log((app.frame/app.fps * 1000)|0);
     },
 });
 
@@ -96,7 +105,7 @@ tm.define("Block", {
     superClass: "tm.app.Shape",
     init: function() {
         this.superInit(BLOCK_SIZE, BLOCK_SIZE);
-        this.type = TYPE.NONE;//0で無し 1で壁 2以降ブロック
+        this.type = TYPE.NONE;//0で無し 1で壁 2ブロック
         this.color = 0;
     },
     update: function() {
@@ -111,7 +120,7 @@ tm.define("Block", {
             // }
             //色はswitchでいいかもね
             if (this.color == COLOR.RED) {
-                this.canvas.clearColor("hsl(0, 50%, 70%");
+                this.canvas.clearColor("hsl(0, 50%, 70%)");
             } else if (this.color == COLOR.BLUE) {
             } else if (this.color == COLOR.GREEN) {
             } else if (this.color == COLOR.YELLOW) {
@@ -120,17 +129,46 @@ tm.define("Block", {
     },
 });
 
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
+    return this;
+};
+
 tm.define("Mino", {
-    init: function() {
-        this.status = [];
-        this.color = COLOR.RED;
-        this.flag = false;//地面についてる？
-        this.cnt = 0;
-        this.speed = 60;//落ちるスピード(数値ms事に)
-    },
-    update: function() {
-        this.cnt++;
-        if (this.cnt % this.speed == 0) {
+    init: function(point, n, color) {
+        this.point = new Array(n);
+        this.color = color;
+        this.n = n;//パターン数
+
+        for (var i = 0; i < n; i++) {//パターン数
+            this.point[i] = new Point();
+            //とりまコピー
+            for (var j = 0; j < 4; j++) {
+                this.point[i][j] = point[j];
+            }
+            //回転パターンが有る場合は生成
+            // for (var j = 0; j < MINO_NUM; j++) {
+            // }
         }
     },
+    update: function() {
+    },
 });
+
+// tm.define("Mino", {
+//     init: function() {
+//         //this.status = [];
+//         this.color = COLOR.RED;
+//         this.flag = false;//地面についてる？
+//         this.cnt = 0;
+//         this.speed = 60;//落ちるスピード(数値ms事に)
+//         this.x;
+//         this.y;
+//     },
+//     update: function() {
+//         this.cnt++;
+//         if (this.cnt % this.speed == 0) {
+//         }
+//     },
+// });

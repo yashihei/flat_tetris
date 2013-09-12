ecc3c7e6d69970a5e08169aa10f00be21dbbd51a
yashihei <yashihei@gmail.com>
2013-09-13 01:49:00 +0900

    何したか忘れたけど、とりあえずミノがいい感じに動いてるかも！！

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 /*
- * 何か作らないと、死んでしまいそうだから
+ * js!!!!!!!!!!!!!!!
  */
 
 /*
  * 定数
  */
-var SCREEN_WIDTH    = 400;
-var SCREEN_HEIGHT   = 440;
-var SCREEN_CENTER_X = SCREEN_WIDTH/2;
-var SCREEN_CENTER_Y = SCREEN_HEIGHT/2;
 
 var BLOCK_NUM_X = 12;//壁のぶんも
 var BLOCK_NUM_Y = 21;
@@ -19,8 +15,15 @@ var MINO_NUM_X = 4;
 var MINO_NUM_Y = 4;
 var MINO_NUM = 16;
 
-var COLOR = {RED: 0, BLUE: 1, GREEN: 2, YELLOW: 3};
-var TYPE = {NONE: 0, WALL: 1, BLOCK: 2};
+var SCREEN_WIDTH    = BLOCK_NUM_X * BLOCK_SIZE + BLOCK_NUM_X - 1;
+var SCREEN_HEIGHT   = BLOCK_NUM_Y * BLOCK_SIZE + BLOCK_NUM_Y - 1;
+var SCREEN_CENTER_X = SCREEN_WIDTH/2;
+var SCREEN_CENTER_Y = SCREEN_HEIGHT/2;
+
+var COLOR = {BROWN: 0, RED: 1, BLUE: 2, GREEN: 3, YELLOW: 4, NONE: 10};
+//var TYPE = {NONE: 0, WALL: 1, BLOCK: 2};
+
+var SQS_NUM = 4;
 
 /*
  * メイン処理(ページ読み込み後に実行される)
@@ -45,130 +48,176 @@ tm.define("MainScene", {
         // 親の初期化
         this.superInit();
 
-        this.blockGroup = tm.app.CanvasElement();
-        this.addChild(this.blockGroup);
+        var blockGroup = tm.app.CanvasElement();
+        this.addChild(blockGroup);
 
-        this.cnt = 0;//カウントでふ
-        this.blocks = new Array();
+        var blocks = [];
 
         //ブロック初期化
         for (var i = 0; i < BLOCK_NUM_Y; ++i) {
-            this.blocks[i] = new Array();
+            blocks[i] = [];
             for (var j = 0; j < BLOCK_NUM_X; ++j) {
-                this.blocks[i][j] = Block().addChildTo(this.blockGroup);
+                blocks[i][j] = Block().addChildTo(blockGroup);
                 if (j == 0 || j == BLOCK_NUM_X - 1 || i == BLOCK_NUM_Y - 1) {
-                    this.blocks[i][j].type = TYPE.WALL;
+                    blocks[i][j].color = COLOR.BROWN;//壁
                 }
                 //座標は左上ではなく中心っぽいのです ていうかサンプルの星で気づくべきだった…
                 //sprite.origin.x sprite.origin.yを設定することで左上を頂点とできるみたい
-                this.blocks[i][j].x = (BLOCK_SIZE + 1) * j + BLOCK_SIZE/2;
-                this.blocks[i][j].y = (BLOCK_SIZE + 1) * i + BLOCK_SIZE/2;
+                blocks[i][j].x = (BLOCK_SIZE + 1) * j + BLOCK_SIZE/2;
+                blocks[i][j].y = (BLOCK_SIZE + 1) * i + BLOCK_SIZE/2;
             }
         }
 
-        //this.mino = Mino(new Array(new Point(0, -1), new Point(1, -1), new Point(0, 0), new Point(1, 1)), 1, COLOR.RED);
-        this.minos = new Array(
-            Mino(new Array(new Point(0, -1), new Point(1, -1), new Point(0, 0), new Point(1, 0)), 1, COLOR.RED)
-        );
+        var minos = [
+            Mino([Point(0, -1), Point(1, -1), Point(0, 0), Point(1, 0)], 1, COLOR.RED),
+        ];
 
-        //ミノの場所
-        this.pos = new Point(4, 1);
+        //ミノの状態
+        var pos = Point(4, 1);
+        //this.mino = this.minos[tm.util.Random.randint(0, 6)];
+        var mino = minos[0];
+        //this.rot = tm.util.Random.randint(0, this.block.n);
+        var rot = 0;
+        var fallCnt = 0;
 
         // 星スプライト
-        this.star = tm.app.StarShape(64, 64);
-        this.addChild(this.star);    // シーンに追加
-    },
+        var star = tm.app.StarShape(64, 64);
+        this.addChild(star);    // シーンに追加
 
-    update: function(app) {
-        var p = app.pointing;
-        // マウス位置 or タッチ位置に移動
-        this.star.x = p.x;
-        this.star.y = p.y;
-        // クリック or タッチ中は回転させる
-        if (app.pointing.getPointing() == true) {
-            this.star.rotation += 15;
+        this.update = function(app) {
+            var p = app.pointing;
+
+            //var star = this.star;//おぶじぇくとはさんｓにょう
+            star.x = p.x;
+            star.y = p.y;
+            // クリック or タッチ中は回転させる
+            if (p.getPointing() == true) {
+                star.rotation += 15;
+            }
+
+            //ここからテトリス
+            fallCnt++;
+            var nextPos = Point(pos.x, pos.y);
+            var nextRot = rot;
+
+            var key = app.keyboard;
+
+            if ((fallCnt + 1) % 60 == 0) {
+                nextPos.y++;
+            } else if (key.getKeyDown("left")) {
+                nextPos.x--;
+            } else if (key.getKeyDown("right")) {
+                nextPos.x++;
+            } else if (key.getKeyDown("down")) {
+                nextPos.y++;
+            } else if (key.getKeyDown("up")) {//回転
+            }
+
+            var sqs = [];
+            var nextSqs = [];
+
+            this.getSqs(mino, pos, rot, sqs, blocks);//いまいるところね
+            this.putSqs(blocks, sqs, COLOR.NONE);//今いるところを黒くぬるのだっだだ
+
+            if (this.getSqs(mino, nextPos, nextRot, nextSqs, blocks)) {
+                this.putSqs(blocks, nextSqs, mino.color);
+                pos = nextPos;
+                rot = nextRot;
+            } else {
+                this.putSqs(blocks, sqs, mino.color);
+            }
+
+            // for (var i = 0; i < SQS_NUM; i++) {
+            //     nextSqs[i] = Point(pos.x + mino.sq[rot][i].x, pos.y + mino.sq[rot][i].y);
+            // }
+
+            // for (var i = 0; i < SQS_NUM; i++) {
+            //     blocks[nextSqs[i].y][nextSqs[i].x].color = COLOR.RED;
+            // }
         }
-
-        // this.cnt++;
-        // var nextPos = new Point(this.pos.x, this.pos.y);
-        // if ((this.cnt + 1) % 60 == 0) nextPos.y++;
-
-        // for (var i = 0; i < 4; i++) {
-        // }
-
-        //var time = ((app.frame / app.fps) * 1000)|0;
-        //console.log((app.frame/app.fps * 1000)|0);
     },
+
+    getSqs: function(mino, pos, rot, sqs, blocks) {//今のミノとミノの位置とパターンを教えて、sqsを得ましょう！
+        var overlap = false;
+        for (var i = 0; i < SQS_NUM; i++) {
+            var p = Point(pos.x + mino.sq[rot][i].x, pos.y + mino.sq[rot][i].y);
+            overlap |= blocks[p.y][p.x].color == COLOR.BROWN;
+            sqs[i] = p;
+        }
+        return !overlap;
+    },
+
+    putSqs: function(blocks, sqs, color) {
+        for (var i = 0; i < SQS_NUM; i++) {
+            blocks[sqs[i].y][sqs[i].x].color = color;
+        }
+    },
+
+    // update: function(app) {
+    //this天国やよ！！！！！！１
+    //     var p = app.pointing;
+    //     // マウス位置 or タッチ位置に移動
+    //     this.star.x = p.x;
+    //     this.star.y = p.y;
+    //     // クリック or タッチ中は回転させる
+    //     if (app.pointing.getPointing() == true) {
+    //         this.star.rotation += 15;
+    //     }
+
+    //     var nextPos = Point(this.pos.X, this.pos.Y);
+    //     var nextRot = this.rot;
+
+    // },
 });
 
 tm.define("Block", {
     superClass: "tm.app.Shape",
     init: function() {
         this.superInit(BLOCK_SIZE, BLOCK_SIZE);
-        this.type = TYPE.NONE;//0で無し 1で壁 2ブロック
-        this.color = 0;
+        this.color = COLOR.NONE;
     },
     update: function() {
-        if (this.type == TYPE.WALL) {
-            this.canvas.clearColor("hsl(0, 0%, 70%)");//壁は灰色で
-        } else if (this.type == TYPE.BLOCK) {
-            // switch (this.color) {
-            //     case COLOR.RED:
-            //     case COLOR.BLUE:
-            //     case COLOR.GREEN:
-            //     case COLOR.YELLOW:
-            // }
-            //色はswitchでいいかもね
-            if (this.color == COLOR.RED) {
-                this.canvas.clearColor("hsl(0, 50%, 70%)");
-            } else if (this.color == COLOR.BLUE) {
-            } else if (this.color == COLOR.GREEN) {
-            } else if (this.color == COLOR.YELLOW) {
-            }
+        if (this.color == COLOR.NONE) {
+            this.canvas.clear();
+            return;
         }
+        var h, s;
+        if(this.color != COLOR.BROWN) {
+            s = 50;
+        } else {
+            s = 0;
+        }
+        switch (this.color) {
+            case COLOR.BROWN:
+            case COLOR.RED:
+                h = 0; break;
+        }
+        this.canvas.clearColor("hsl({0}, {1}%, 70%)".format(h, s));
     },
 });
 
-function Point(x, y) {
-    this.x = x;
-    this.y = y;
-    return this;
-};
+tm.define("Point", {
+    init: function(x, y) {
+        this.x = x;
+        this.y = y;
+    },
+})
 
 tm.define("Mino", {
     init: function(point, n, color) {
-        this.point = new Array(n);
+        this.sq = [];
         this.color = color;
         this.n = n;//パターン数
 
         for (var i = 0; i < n; i++) {//パターン数
-            this.point[i] = new Point();
-            //とりまコピー
-            for (var j = 0; j < 4; j++) {
-                this.point[i][j] = point[j];
+            this.sq[i] = [];
+            for (var j = 0; j < SQS_NUM; j++) {
+                this.sq[i][j] = point[j];
+                this.sq[i][j] = this.rotate(this.sq[i][j]);//回転
             }
-            //回転パターンが有る場合は生成
-            // for (var j = 0; j < MINO_NUM; j++) {
-            // }
         }
     },
-    update: function() {
+    rotate: function(p) {
+        return Point(p.y, -p.x);
     },
 });
-
-// tm.define("Mino", {
-//     init: function() {
-//         //this.status = [];
-//         this.color = COLOR.RED;
-//         this.flag = false;//地面についてる？
-//         this.cnt = 0;
-//         this.speed = 60;//落ちるスピード(数値ms事に)
-//         this.x;
-//         this.y;
-//     },
-//     update: function() {
-//         this.cnt++;
-//         if (this.cnt % this.speed == 0) {
-//         }
-//     },
-// });
